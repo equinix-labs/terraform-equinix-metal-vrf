1b77d19e2b5548f0b58eb673eff7acda96f4d8a9
larrymapacket <46980377+larrymapacket@users.noreply.github.com>
2022-07-30 10:41:06 -0500

    Update outputs.tf to only show relevant information

--- outputs.tf
+++ outputs.tf
@@ -8,10 +8,12 @@ output "server_name" {
   description = "Your metal node's hostname:"
 }
 
+/* Don't output metal nodes' IPs in layer2 mode
 output "metal_node_ip" {
   value       = module.metal_nodes.nodes_public_ipv4
   description = "Your metal node's IP addresses:"
 }
+*/
 
 output "ssh_private_key" {
   value       = module.ssh.ssh_private_key
@@ -27,18 +29,37 @@ output "metal_gateway" {
   value       = equinix_metal_gateway.my_gateway.*
   description = "The Gateway Information:"
 }
-
-output "dedicated_port" {
-  value       = data.equinix_metal_connection.dedicated_port.*
-  description = "The dedicated port for VRF:"
+output "dedicated_ports" {
+  value       = {
+    port_id    = data.equinix_metal_connection.dedicated_port.id,
+    name       = data.equinix_metal_connection.dedicated_port.name,
+    metro      = data.equinix_metal_connection.dedicated_port.metro,
+    redundancy = data.equinix_metal_connection.dedicated_port.redundancy
+  }
+  description = "The dedicated port information for VRF"
 }
 
 output "virtual_connection_primary" {
-  value       = equinix_metal_virtual_circuit.my_vc_pri.*
-  description = "Information of Primary VC connecting to VRF:"
+  value       = {
+    vc_id     = equinix_metal_virtual_circuit.my_vc_pri.connection_id,
+    name      = equinix_metal_virtual_circuit.my_vc_pri.name,
+    nni_vlan  = equinix_metal_virtual_circuit.my_vc_pri.nni_vlan,
+    peer_asn  = equinix_metal_virtual_circuit.my_vc_pri.peer_asn,
+    peer_ip   = equinix_metal_virtual_circuit.my_vc_pri.customer_ip,
+    metal_ip  = equinix_metal_virtual_circuit.my_vc_pri.metal_ip
+  }
+  description = "Information of Primary VC connecting to VRF"
 }
 
 output "virtual_connection_secondary" {
-  value       = equinix_metal_virtual_circuit.my_vc_sec.*
-  description = "Information of Secondary VC connecting to VRF:"
+  value       = {
+    vc_id     = equinix_metal_virtual_circuit.my_vc_sec.connection_id,
+    name      = equinix_metal_virtual_circuit.my_vc_sec.name,
+    nni_vlan  = equinix_metal_virtual_circuit.my_vc_sec.nni_vlan,
+    peer_asn  = equinix_metal_virtual_circuit.my_vc_sec.peer_asn,
+    peer_ip   = equinix_metal_virtual_circuit.my_vc_sec.customer_ip,
+    metal_ip  = equinix_metal_virtual_circuit.my_vc_sec.metal_ip
+  }
+  description = "Information of Secondary VC connecting to VRF"
 }
+
